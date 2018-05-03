e72adef8dd73ae982d8876fbac4a58b2d70d6603
Sagar Tamang <mi5t4n@gmail.com>
2018-05-03 14:16:47 +0545

    train front end implemented

--- cnn/flaskr/__init__.py
+++ cnn/flaskr/__init__.py
@@ -1,7 +1,16 @@
-import os
+import os, uuid
+from flask import (
+    Flask, render_template, url_for, redirect,
+    request, flash
+)
+from werkzeug.utils import secure_filename
 
-from flask import Flask, render_template, url_for, redirect
+UPLOAD_FOLDER = '/home/kazekage/websites/image-classification/cnn/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def create_app(test_config=None):
     # create and configure the app
@@ -10,6 +19,7 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -29,12 +39,40 @@ def create_app(test_config=None):
     def hello():
         return render_template('home.html')
     
-    @app.route('/train')
+    @app.route('/train', methods=('GET','POST'))
     def train():
-        return render_template('train.html')
+        if request.method == 'GET':
+            return render_template('train.html')
+        elif request.method == 'POST':
+            print (request.form.getlist('label[]'))
+            print (request.files.getlist('sagar'))
+
+            # Creating a random directory to hold the images
+            directory = UPLOAD_FOLDER + '/' + str(uuid.uuid4())
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            
+            # Creating directories for labels and saving images
+            labels = request.form.getlist('label[]')
+            for label in labels:
+                label_dir = directory + '/' + label
+                if not os.path.exists(label_dir):
+                    os.makedirs(label_dir)
+                # Saving images to the label directory
+                files = request.files.getlist(label)
+                for file in files:
+                    if file and allowed_file(file.filename):
+                        filename = secure_filename(file.filename) 
+                        print (filename)
+                        file.save(os.path.join(label_dir, filename))
+
     
     @app.route('/test')
     def test():
         return render_template('test.html')
+
+    @app.route('/process_train')
+    def process_train():
+        pass
 
     return app
--- cnn/flaskr/static/train.js
+++ cnn/flaskr/static/train.js
@@ -1,18 +1,77 @@
+
 $(document).ready(function(){
+  var isDirty = true;
+
+  function changeFileInputName(){
+    var labels = [];
+  
+    // Get labels
+    $("input[type='text']").each(function(index,element){
+      labels.push($(this).val());
+      console.log($(this).val());
+    });
+  
+    //Reverse the labels
+    labels.reverse();
+  
+    //Set labels of file uploads
+    $("input[type='file']").each(function(index,element){
+      $(this).attr('name',labels.pop());
+    });
+
+    isDirty = false;
+  }
+
   $('#add').click(function(e){
     var lbl_img_html = $("#label_image").html();
     $("#label_image").append(lbl_img_html);
-    console.log(lbl_img_html);
+    // console.log(lbl_img_html);
   });
 
   $("#remove").click(function(e){
-    console.log($("#label_image"));
+    // console.log($("#label_image"));
     var label_image = document.querySelector('#label_image');
     var num_of_child_nodes = label_image.childNodes.length;
     if (num_of_child_nodes == 3) return;
     label_image.removeChild(label_image.childNodes[num_of_child_nodes-1]);
     label_image.removeChild(label_image.childNodes[num_of_child_nodes-2]);
     label_image.removeChild(label_image.childNodes[num_of_child_nodes-3]);
+  });
+
+  $('#apply').click(function(e){
+    changeFileInputName();
+  });
+
+  // Form Upload Handle
+  $("div.progress").hide();
+  var progress_bar = $('div.progress-bar');
+  var status = $('#status');
+
+  $('form').ajaxForm({
+      beforeSubmit: function(arr, $form, options){
+        if (isDirty) {
+          $('#myModal').modal('toggle')
+          return false;
+        }
+      },
+      beforeSend: function() {
+        console.log('BeforeSend');
+        $("div.progress").show();
+          status.empty();
+          var percentVal = 0;
+          progress_bar.attr("aria-valuenow", percentVal);
+          progress_bar.attr("style","width: "+percentVal+"%");
+          progress_bar.html(percentVal+'%');
+      },
+      uploadProgress: function(event, position, total, percentComplete) {
+          var percentVal = percentComplete;
+          progress_bar.attr("aria-valuenow", percentVal);
+          progress_bar.attr("style","width: "+percentVal+"%");
+          progress_bar.html(percentVal+'%');
+      },
+      complete: function(xhr) {
+          status.html(xhr.responseText);
+      }
   });
 });
 
